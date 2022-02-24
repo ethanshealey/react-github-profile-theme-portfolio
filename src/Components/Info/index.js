24576ad1c1aeeaf0d91f5eed490c0cbd7644eee0
Ethan Shealey <ethan.shealey@gmail.com>
2022-02-23 20:02:39 -0600

    fix

--- src/Components/Info/index.js
+++ src/Components/Info/index.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import { Button, Icon } from 'antd'
+import { useEffect } from 'react'
+import { Button, Grid } from 'antd'
 import { MdOutlineMailOutline } from 'react-icons/md'
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
+const { useBreakpoint } = Grid
+
 const Info = () => {
 
+  const screens = useBreakpoint()
+
+  useEffect(() => { console.log(screens) }, [screens])
+
   return (
-    <>
-        <img className="avatar" src='../images/pp.jpg' alt="logo" />
+    <div style={{ marginTop: screens['md'] ? 0 : '50px' }}>
+        <img className="avatar" src='../images/pp.jpg' alt="logo"/>
         <h1 className="name">Ethan Shealey</h1>
         <p className="username">ethanshealey</p>
         <a href="../../static/EthanShealeyResume.pdf" download><Button className="cv">Download CV</Button></a>
         <div className="email"><MdOutlineMailOutline className="icon" /> <a className="anchor" href="mailto:ethan.shealey@gmail.com" target="_blank">ethan.shealey@gmail.com</a></div>
         <div className="link"><AiFillGithub className="icon" /> <a className="anchor" href="https://www.github.com/ethanshealey" target="_blank">github.com/ethanshealey</a></div>
         <div className="link"><AiFillLinkedin className="icon" /> <a className="anchor" href="https://www.linkedin.com/in/ethan-shealey" target="_blank">linkedin.com/in/ethan-shealey</a></div>
-   </>
+   </div>
   )
 }
 
